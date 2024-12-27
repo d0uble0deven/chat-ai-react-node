01245d99359546810af88ff1e34e4af0d01ba71c
Dev Govindji <dev.govindji.software@gmail.com>
2024-12-27 14:54:45 -0600

    Fix: Update paths for renamed app directory

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.get("/api/health", (req, res) => {
 
 // Catch-all handler for React routing
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/build", "index.html"));
+  res.sendFile(path.join(__dirname, "app/build", "index.html")); // Updated path
 });
 
 // Start the server
